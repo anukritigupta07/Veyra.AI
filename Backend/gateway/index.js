e77e0408eb0a70e3b41ed9b3f217d7ed4760676f
Anukriti Gupta <anukritigupta12@outlook.com>
2026-09-25 09:27:18 +0530

    Creating Server

---
+++ Backend/gateway/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+
+const port = process.env.PORT || 7000;
+const app = express();
+
+app.listen(port ,() => {
+    console.log(`Server is running on port ${
+        port}`);
+})
