43f54c506a4530ee59ece4cae7ae25c3ea2cecc3
Anukriti Gupta <anukritigupta12@outlook.com>
2026-09-25 09:47:58 +0530

    auth server setup

--- Backend/services/auth/index.js
+++ Backend/services/auth/index.js
@@ -4,14 +4,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 
-const port = process.env.PORT || 7000;
+const port = process.env.PORT || 7001;
 const app = express();
 
 app.get("/", (req, res) => {
-    res.json({ message: "Gateway is running" });
+    res.json({ message: "Auth service is running" });
 });
 
 app.listen(port ,() => {
-    console.log(`Gateway started ${
+    console.log(`auth started on port ${
         port}`);
 })
